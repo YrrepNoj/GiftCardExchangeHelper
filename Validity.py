b4d1793f2dc187c0549b2fbc86be419c0a1c0d7e
Jon Perry <jperry16@gmu.edu>
2018-11-13 20:21:21 -0500

    Added check to ensure submission was not already closed

--- Validity.py
+++ Validity.py
@@ -8,10 +8,18 @@ hitWords = ["amazon", "amz"]
 # Extremely basic check to determine if a post if valid
 # TODO: Improve upon this logic
 def determineValidity(submission):
-	opTitle = submission.title.lower()
-	opTitle = opTitle.split("[w]")[0]
 
-	if any(string in opTitle for string in hitWords):
-		logging.info("Found a valid post: %s", opTitle)
-		return True
-	return True
+    opFlair = submission.link_flair_text
+    opTitle = submission.title.lower()
+    opTitle = opTitle.split("[w]")[0]
+
+    # Check to ensure the exchange hasn't already been completed
+    if opFlair is not None and opFlair.lower() == "closed":
+        return False
+
+    # Check to see if any of the hitWords are contained in the title
+    if any(string in opTitle for string in hitWords):
+        logging.info("Found a valid post: %s", opTitle)
+        return True
+
+    return False
