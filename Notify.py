8c645e724a99a327ee0c4e88141657ad15ce4498
Jon Perry <jperry16@gmu.edu>
2018-11-14 21:19:10 -0500

    Added ability to send notification email

--- Notify.py
+++ Notify.py
@@ -1,15 +1,31 @@
 import praw
 from AccountInfo import *
 import logging
+import smtplib
 
 def notifyMaster(redditClient, validPosts):
-	finalMessage = ""
+    finalMessage = ""
 
-	for validPost in validPosts:
-		finalMessage += '[' + validPost.title.lower() + '] (%s)\n\n' % validPost.url
+    for validPost in validPosts:
+        finalMessage += '[' + validPost.title.lower() + '] (%s)\n\n' % validPost.url
 
-	print (finalMessage)
-	redditClient.redditor(masterUser).message("ValidPosts",  finalMessage)
+    redditClient.redditor(masterUser).message("ValidPosts",  finalMessage)
 
-	logging.info("Notifying master with %d new valid posts", len(validPosts))
-	return finalMessage
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(gmailUser, gmailPassword)
+
+        emailText = """From: %s\nTo: %s\nSubject: %s\n\n%s
+        """ % (gmailUser, gmailReceiver, "ValidPosts", finalMessage)
+        server.sendmail(gmailUser, gmailReceiver, emailText)
+        server.close()
+
+        logging.info("Sent email and closed server")
+    except:
+        logging.warn("Unable to connect to smtplib server")
+
+    logging.info("Notifying master with %d new valid posts", len(validPosts))
+    return finalMessage
