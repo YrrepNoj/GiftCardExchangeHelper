20664010003e6c67fe6ee8d9798aeb04bc0dc3f0
yrrepnoj <yrrepnoj@gmail.com>
2020-01-15 21:26:14 -0500

    added placeholder accountInfo file

---
+++ AccountInfo.py
@@ -0,0 +1,8 @@
+redditUsername = "REDDIT_USERNAME_STRING"
+redditPassword = "REDDIT_PASSWORD_STRING"
+clientSecret = "REDDIT_CLIENT_SECRET_STRING"
+clientID = "REDDIT_CLIENT_STRING" #https://www.reddit.com/prefs/apps
+
+gmailUser = "GMAIL_EMAIL_STRING"
+gmailPassword = "GMAIL_PASSWORD_STRING"
+masterUser = None
